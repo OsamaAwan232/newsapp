88ba58dbe0c8664d23204276c2d302130176740e
Osama Awan <osamaawan768@gmail.com>
2024-04-15 13:32:50 +0500

    Create Components Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>This is class based component</h1>
+        <Navbar/>
+        <News/>
       </div>
     )
   }
